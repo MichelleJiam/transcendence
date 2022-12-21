ca83d2c15296ab7a541f1e6c20a34d790ccf13ea
Niks Hariman <nhariman@hotmail.com>
2022-12-21 16:40:33 -0500

    fixed backend linter errors

--- backend/app/src/message/Message.controller.ts
+++ backend/app/src/message/Message.controller.ts
@@ -1,39 +1,38 @@
 import {
-	Controller,
-	Post,
-	Get,
-	Body,
-	ParseIntPipe,
-	Param,
-  } from "@nestjs/common";
-  import { CreateMessageDto } from "./dto/CreateMessage.dto";
-  import { MessageService } from "./Message.service";
-  
-  // @Param() converts what's inside the uri that starts with : into a variable
-  // @Body() tells nest to save whatever's in the body to a particular variable. In this case, a dto class.
-  
-  @Controller("message")
-  export class MessageController {
-	constructor(private readonly messageService: MessageService) {}
-  
-	@Get()
-	getAllMessages() {
-	  return this.messageService.getAllMessages();
-	}
-  
-	@Get("user/id/:id")
-	getMessageById(@Param("id", ParseIntPipe) id: number) {
-	  return this.messageService.getMessageByUserId(id);
-	}
-  
-	@Get("user/:username")
-	getMessageByUsername(@Param("username") username: string) {
-	  return this.messageService.getMessageByUsername(username);
-	}
-  
-	@Post("create")
-	create(@Body() createMessageDto: CreateMessageDto) {
-	  return this.messageService.create(createMessageDto);
-	}
+  Controller,
+  Post,
+  Get,
+  Body,
+  ParseIntPipe,
+  Param,
+} from "@nestjs/common";
+import { CreateMessageDto } from "./dto/CreateMessage.dto";
+import { MessageService } from "./Message.service";
+
+// @Param() converts what's inside the uri that starts with : into a variable
+// @Body() tells nest to save whatever's in the body to a particular variable. In this case, a dto class.
+
+@Controller("message")
+export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @Get()
+  getAllMessages() {
+    return this.messageService.getAllMessages();
   }
-  
+
+  @Get("user/id/:id")
+  getMessageById(@Param("id", ParseIntPipe) id: number) {
+    return this.messageService.getMessageByUserId(id);
+  }
+
+  @Get("user/:username")
+  getMessageByUsername(@Param("username") username: string) {
+    return this.messageService.getMessageByUsername(username);
+  }
+
+  @Post("create")
+  create(@Body() createMessageDto: CreateMessageDto) {
+    return this.messageService.create(createMessageDto);
+  }
+}
--- backend/app/src/message/Message.entity.ts
+++ backend/app/src/message/Message.entity.ts
@@ -1,35 +1,34 @@
 import {
-	Column,
-	CreateDateColumn,
-	Entity,
-	JoinColumn,
-	ManyToOne,
-	PrimaryGeneratedColumn,
-  } from "typeorm";
-  import { User } from "src/user/User.entity";
-  import { Chatroom } from "src/chat/Chat.entity";
-  
-  @Entity()
-  export class Message {
-	@PrimaryGeneratedColumn({
-	  name: "message_id",
-	})
-	public id?: number;
-  
-	@Column({
-	  nullable: false,
-	})
-	public body!: string;
-  
-	@CreateDateColumn()
-	created_at!: Date;
-  
-	@ManyToOne(() => User, (userId: User) => userId.messages)
-	@JoinColumn()
-	public userId!: User;
-  
-	@ManyToOne(() => Chatroom, (chatroomId: Chatroom) => chatroomId.messages)
-	@JoinColumn()
-	public chatroomId!: Chatroom;
-  }
-  
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { User } from "src/user/User.entity";
+import { Chatroom } from "src/chat/Chat.entity";
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn({
+    name: "message_id",
+  })
+  public id?: number;
+
+  @Column({
+    nullable: false,
+  })
+  public body!: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @ManyToOne(() => User, (userId: User) => userId.messages)
+  @JoinColumn()
+  public userId!: User;
+
+  @ManyToOne(() => Chatroom, (chatroomId: Chatroom) => chatroomId.messages)
+  @JoinColumn()
+  public chatroomId!: Chatroom;
+}
