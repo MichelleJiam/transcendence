3966ab62e984b5a980403d9f905e16ece79b1687
NHariman <NHariman@users.noreply.github.com>
2022-12-21 17:58:17 +0100

    Delete chat.module.ts

--- backend/app/src/chat/chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ChatController } from "./Chat.controller";
-import { ChatService } from "./Chat.service";
-
-@Module({
-  controllers: [ChatController],
-  providers: [ChatService],
-})
-export class ChatModule {}
