10c6a2282a3c30f8c12cbae4e759cfb4e1df4873
NHariman <NHariman@users.noreply.github.com>
2022-12-21 17:58:09 +0100

    Delete chat.controller.ts

--- backend/app/src/chat/chat.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Post, Get, Body } from "@nestjs/common";
-import { ChatService } from "./Chat.service";
-
-@Controller("chat")
-export class ChatController {
-  constructor(private readonly chatService: ChatService) {}
-}
