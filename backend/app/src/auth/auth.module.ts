cca9fdd839420d162a3be9966aa9f671800cf977
Michelle Jiam <mjiam@student.codam.nl>
2023-01-04 20:40:36 +0100

    TODO: figure out how to use JWT token as cookie

--- backend/app/src/auth/auth.module.ts
+++ backend/app/src/auth/auth.module.ts
@@ -1,19 +1,18 @@
-import { UserService } from "./../user/User.service";
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./jwt.strategy";
-import { User } from "../user/User.entity";
-import { UserController } from "../user/User.controller";
+import { User } from "../user/user.entity";
+import { UserController } from "../user/user.controller";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
       useFactory: () => ({
-        secret: process.env.PRIVATE_KEY,
+        secret: process.env.JWT_SECRET,
         signOptions: {
           expiresIn: "60m",
         },
--- backend/app/src/auth/jwt.strategy.ts
+++ backend/app/src/auth/jwt.strategy.ts
@@ -1,26 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { FindOneOptions, Repository } from "typeorm";
-import { User } from "../user/User.entity";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {
+  constructor() {
     super({
       // gets bearer token from request headers
       jwtfromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.PRIVATE_KEY,
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
-  async validate(payload: { sub: FindOneOptions<User> }) {
-    const userId = payload.sub;
-    return await this.userRepository.findOne(userId);
+  async validate(payload: any) {
+    return { id: payload.sub, intraId: payload.intraId };
   }
 }
