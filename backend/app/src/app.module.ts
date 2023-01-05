f3e62d7b3eb3c1a0673c43562c2f417a4ddb3c86
Niks Hariman <nhariman@hotmail.com>
2023-01-05 15:57:10 +0100

    resolved merge conflicts

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -1,15 +1,12 @@
-import { Module } from "@nestjs/common";
+ßimport { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MessageModule } from "./message/message.module";
 import { UserModule } from "./user/user.module";
 import { DatabaseModule } from "./database.module";
-<<<<<<< HEAD
 import { ChatModule } from "./chat/chat.module";
-=======
 import * as Joi from "joi";
->>>>>>> 3ed81b92ae4fc9f3eae939aec641943b3b41864f
 
 @Module({
   imports: [
