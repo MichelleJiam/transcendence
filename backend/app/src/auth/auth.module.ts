02e72b4592b045af164186372e94a8915676ab98
Michelle Jiam <mjiam@student.codam.nl>
2023-01-05 17:54:54 +0100

    Added algorithm specification in jwt sign options

--- backend/app/src/auth/auth.module.ts
+++ backend/app/src/auth/auth.module.ts
@@ -14,6 +14,7 @@ import { UserController } from "../user/user.controller";
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
         signOptions: {
+          algorithm: "HS256",
           expiresIn: "60m",
         },
       }),
