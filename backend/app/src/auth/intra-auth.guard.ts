776fd90f7eb93587ba03f0fc27cb5e8532627c2d
Michelle Jiam <mjiam@student.codam.nl>
2023-01-06 16:15:17 +0100

    Added canActivate fn and fixed request.login error by putting canActivate call before login call

--- backend/app/src/auth/intra-auth.guard.ts
+++ backend/app/src/auth/intra-auth.guard.ts
@@ -1,3 +1,12 @@
+import { ExecutionContext, Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
-export class IntraAuthGuard extends AuthGuard("42") {}
+@Injectable()
+export class IntraAuthGuard extends AuthGuard("42") {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const result = (await super.canActivate(context)) as boolean;
+    super.logIn(request);
+    return result;
+  }
+}
