43b606ed2c1d686aa805aaff45afaec582b8637a
Niks Hariman <nhariman@hotmail.com>
2023-01-06 18:14:15 +0100

    fixing bans

--- backend/app/src/chat/dto/punish-user.dto.ts
+++ backend/app/src/chat/dto/punish-user.dto.ts
@@ -2,9 +2,10 @@ import { IsNotEmpty } from "class-validator";
 import { User } from "src/user/user.entity";
 import { DeepPartial } from "typeorm";
 import { Chatroom } from "../chat.entity";
-import { UpdateChatroomDto } from "./update-chat.dto";
+import { Penalty } from "../penalty.entity";
+export class BanUserDto {
+  ban!: Penalty;
 
-export class BanUserDto extends UpdateChatroomDto {
   @IsNotEmpty()
   userId!: DeepPartial<User[]>;
 
