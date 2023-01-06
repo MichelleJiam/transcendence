c6b676b78debe0111e812a50013fa87d774a2287
Michelle Jiam <mjiam@student.codam.nl>
2023-01-06 18:05:10 +0100

    Intra login seems to be working

--- backend/app/src/auth/intra-auth.guard.ts
+++ backend/app/src/auth/intra-auth.guard.ts
@@ -3,10 +3,10 @@ import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class IntraAuthGuard extends AuthGuard("42") {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const result = (await super.canActivate(context)) as boolean;
-    super.logIn(request);
-    return result;
-  }
+  // async canActivate(context: ExecutionContext): Promise<boolean> {
+  //   const request = context.switchToHttp().getRequest();
+  //   const result = (await super.canActivate(context)) as boolean;
+  //   super.logIn(request);
+  //   return result;
+  // }
 }
