a661df930ca502ea59da1bb765369d0bd3e5387a
Niks Hariman <nhariman@hotmail.com>
2023-01-06 18:14:31 +0100

    revert punish-user

--- backend/app/src/chat/dto/punish-user.dto.ts
+++ backend/app/src/chat/dto/punish-user.dto.ts
@@ -2,10 +2,7 @@ import { IsNotEmpty } from "class-validator";
 import { User } from "src/user/user.entity";
 import { DeepPartial } from "typeorm";
 import { Chatroom } from "../chat.entity";
-import { Penalty } from "../penalty.entity";
 export class BanUserDto {
-  ban!: Penalty;
-
   @IsNotEmpty()
   userId!: DeepPartial<User[]>;
 
