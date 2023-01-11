67dcef23ae539492d71bf5248d19adbc8447b90d
Michelle Jiam <mjiam@student.codam.nl>
2023-01-11 20:56:12 +0100

    Intra strategy and guard works for logging in

--- backend/app/src/auth/intra-auth.guard.ts
+++ backend/app/src/auth/intra-auth.guard.ts
@@ -4,9 +4,9 @@ import { AuthGuard } from "@nestjs/passport";
 @Injectable()
 export class IntraAuthGuard extends AuthGuard("42") {
   // async canActivate(context: ExecutionContext): Promise<boolean> {
-  //   const request = context.switchToHttp().getRequest();
-  //   const result = (await super.canActivate(context)) as boolean;
-  //   super.logIn(request);
-  //   return result;
+  // const request = context.switchToHttp().getRequest();
+  // const result = (await super.canActivate(context)) as boolean;
+  // // await super.logIn(request);
+  // return result;
   // }
 }
--- backend/app/src/auth/intra.strategy.ts
+++ backend/app/src/auth/intra.strategy.ts
@@ -2,10 +2,16 @@ import { AuthService } from "./auth.service";
 import { Strategy } from "passport-42";
 import { PassportStrategy } from "@nestjs/passport";
 import { Injectable } from "@nestjs/common";
+import { VerifyCallback } from "passport-jwt";
+import { HttpService } from "@nestjs/axios";
+import { firstValueFrom } from "rxjs";
 
 @Injectable()
 export class IntraStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(
+    private readonly authService: AuthService,
+    private httpService: HttpService,
+  ) {
     super({
       authorizationURL: process.env.INTRA_AUTHORIZE,
       clientID: process.env.INTRA_CLIENT_ID,
@@ -13,20 +19,21 @@ export class IntraStrategy extends PassportStrategy(Strategy) {
       callbackURL: process.env.INTRA_CALLBACK,
       scope: "public",
       profileFields: {
-        id: function (obj: any) {
+        id: function (obj: { id: string }) {
           return String(obj.id);
         },
       },
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: any) {
+  async validate(profile: { id: string }) {
     console.log("Attempting to validate user according to IntraStrategy");
     const createUser = {
       intraId: profile.id,
       password: "password", // TODO: remove once 42Auth implemented
     };
 
-    return await this.authService.validateUser(createUser);
+    const user = await this.authService.validateUser(createUser);
+    return user;
   }
 }
