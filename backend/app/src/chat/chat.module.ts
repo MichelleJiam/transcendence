5ede5af6fe5fb150291d9bd69bcc0ce01490a443
Niks Hariman <nhariman@hotmail.com>
2023-01-23 16:38:45 +0100

    added chatroom.spec.ts for testing in jest but that doesn't quite work yet because it doesn't do absolute paths. also fixed penalties

--- backend/app/src/chat/chat.module.ts
+++ backend/app/src/chat/chat.module.ts
@@ -5,6 +5,7 @@ import { AvatarService } from "src/avatar/avatar.service";
 import { Message } from "src/message/message.entity";
 import { MessageService } from "src/message/message.service";
 import { Penalty } from "src/penalty/penalty.entity";
+import { PenaltyMethod } from "src/penalty/penalty.method";
 import { PenaltyService } from "src/penalty/penalty.service";
 import { User } from "src/user/user.entity";
 import { UserService } from "src/user/user.service";
@@ -20,6 +21,7 @@ import { ChatService } from "./chat.service";
   controllers: [ChatController],
   providers: [
     PenaltyService,
+    PenaltyMethod,
     ChatService,
     ChatMethod,
     UserService,
--- backend/app/src/penalty/penalty.module.ts
+++ backend/app/src/penalty/penalty.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PenaltyController } from "./penalty.controller";
 import { Penalty } from "./penalty.entity";
+import { PenaltyMethod } from "./penalty.method";
 import { PenaltyService } from "./penalty.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Penalty])],
   controllers: [PenaltyController],
-  providers: [PenaltyService],
+  providers: [PenaltyService, PenaltyMethod],
 })
 export class PenaltyModule {}
