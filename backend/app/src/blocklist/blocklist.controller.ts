9006423c85baba28d3d922f14af2c5a4b73bf7ac
Niks Hariman <nhariman@hotmail.com>
2023-02-01 16:41:44 +0100

    tested backend blocklist to filter lists

--- backend/app/src/blocklist/blocklist.controller.ts
+++ backend/app/src/blocklist/blocklist.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, NotFoundException } from "@nestjs/common";
+import { Blocklist } from "./blocklist.entity";
 import { BlocklistService } from "./blocklist.service";
 
 @Controller("blocklist")
 export class BlocklistController {
-  constructor(private readonly blocklistService: BlocklistService) {
+  constructor(private readonly blocklistService: BlocklistService) {}
 
-    @Get()
-    async getGlobalBlocklist() {
-      return await this.getGlobalBlocklist();
+  @Get()
+  getGlobalBlocklist(): Promise<Blocklist[]> {
+    try {
+      return this.blocklistService.getGlobalBlocklist();
+    } catch (err) {
+      throw NotFoundException;
     }
   }
 }
--- backend/app/src/blocklist/blocklist.module.ts
+++ backend/app/src/blocklist/blocklist.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { BlocklistController } from "./blocklist.controller";
+import { Blocklist } from "./blocklist.entity";
 import { BlocklistService } from "./blocklist.service";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Blocklist])],
   controllers: [BlocklistController],
   providers: [BlocklistService],
 })
