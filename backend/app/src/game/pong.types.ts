77445780dc8ee501bd9b6144b7e5abd301a4745b
Swaan Miller <subspace@subspace.local>
2023-02-06 08:50:17 +0100

    moved paddle logic to backend, working on ball movement now

--- backend/app/src/game/pong.types.ts
+++ backend/app/src/game/pong.types.ts
@@ -1,7 +1,30 @@
+export interface Canvas {
+  width: number;
+  height: number;
+  offset: number;
+  borderLines: number;
+}
+
 export interface Ball {
-  radius?: number;
-  x?: number;
-  y?: number;
-  moveX?: number;
-  moveY?: number;
+  radius: number;
+  x: number;
+  y: number;
+  moveX: number;
+  moveY: number;
+}
+
+export interface Paddle {
+  height: number;
+  width: number;
+  y: number;
+  offset: number;
+}
+
+export interface GameRoom {
+  room: string;
+  player: string;
+  paddleOne: Paddle;
+  paddleTwo: Paddle;
+  ball: Ball;
+  view: Canvas;
 }
