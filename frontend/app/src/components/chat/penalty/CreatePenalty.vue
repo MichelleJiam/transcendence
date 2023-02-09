26382d9dbe6c0ce826724be640de2cb5c40c6fdd
Niks Hariman <nhariman@hotmail.com>
2023-02-09 22:20:18 +0100

    created penalty maker for frontend, just need to figure out how to send more info from frontend to backend

---
+++ frontend/app/src/components/chat/penalty/CreatePenalty.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import axios from "axios";
+
+const adminId = 1; // replace this with the user Id in the cookie
+
+const penalty = {
+  penaltyType: String,
+  user: Number,
+  chatroom: Number,
+};
+
+function createPenalty() {
+  if (!penalty.penaltyType) {
+    throw new TypeError("faulty ban type");
+  }
+  const url =
+    "http://localhost:3000/chatroom/" + penalty.chatroom + "/admin/" + adminId;
+
+  axios.post(url, penalty).catch((error) => {
+    console.log(error);
+  });
+}
+</script>
