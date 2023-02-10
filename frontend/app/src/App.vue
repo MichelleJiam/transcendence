715605ca8ccdd375313db6f796de879f63613ddd
Michelle Jiam <mjiam@student.codam.nl>
2023-02-10 18:08:49 +0100

    Added auth check to userStore.logOut to fix 401; CORS issue on login

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <!-- <nav v-if="userStore.authenticated === true"> -->
   <nav v-if="useUserStore().authenticated">
+    <!-- <nav v-if="isAuthenticated()"> -->
     <NavBar />
   </nav>
   <RouterView />
 </template>
 
 <script setup lang="ts">
-import NavBar from "./components/NavBar.vue";
-import { useUserStore } from "./stores/UserStore";
+import NavBar from "@/components/NavBar.vue";
+import { useUserStore } from "@/stores/UserStore";
+
+const userStore = useUserStore();
+
+function isAuthenticated() {
+  console.log(
+    "Checking if user authenticated for displaying navbar: ",
+    userStore.authenticated
+  );
+  return userStore.authenticated;
+}
 </script>
