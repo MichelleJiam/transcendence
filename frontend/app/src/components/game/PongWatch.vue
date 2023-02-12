25dd7868f096be47ca300b93111c67e85450c0ce
Swaantje Miller <smiller@f1r1s6.codam.nl>
2023-02-12 15:44:54 +0100

    beginning to write api calls to display all active games

---
+++ frontend/app/src/components/game/PongWatch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="display-content">
+    <p>Watch</p>
+    <button>yo</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Game } from "./pong.types";
+// import apiRequest from "../utils/apiRequest";
+import { ref } from "vue";
+
+const games = ref(Array<Game>());
+
+async function getActiveGames() {
+  await apiRequest(`/game`, "get");
+}
+
+// cycle through games in play
+</script>
+
+<style scoped>
+p {
+  font-family: "ArcadeClassic", sans-serif;
+  font-size: 3vw;
+}
+</style>
