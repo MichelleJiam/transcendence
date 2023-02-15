e49c9ece58794768c6739c535bef01e0b14c2a88
Michelle Jiam <mjiam@student.codam.nl>
2023-02-15 20:40:13 +0100

    Finished backend 2fa logic, need to edit frontend auth flow

--- backend/app/src/2FA/twofactor-auth.controller.ts
+++ backend/app/src/2FA/twofactor-auth.controller.ts
@@ -1,14 +1,17 @@
+import { AuthService } from "./../auth/auth.service";
+import { TwoFactorAuthCodeDto } from "./twofactor-auth-code.dto";
 import { currentUserFromBody } from "./../auth/decorators/current-user.decorator";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 import { TwoFactorAuthService } from "./twofactor-auth.service";
 import { User } from "../user/user.entity";
 import {
+  Body,
   ClassSerializerInterceptor,
   Controller,
-  Get,
+  HttpCode,
   Post,
-  Req,
   Res,
+  UnauthorizedException,
   UseGuards,
   UseInterceptors,
 } from "@nestjs/common";
@@ -17,14 +20,51 @@ import { Response } from "express";
 @Controller("2fa")
 @UseInterceptors(ClassSerializerInterceptor)
 export class TwoFactorAuthController {
-  constructor(private readonly twoFactorAuthService: TwoFactorAuthService) {}
+  constructor(
+    private readonly twoFactorAuthService: TwoFactorAuthService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @Post("generate")
+  @Post("register")
   @UseGuards(JwtAuthGuard)
   async register(@Res() response: Response, @currentUserFromBody() user: User) {
     const { otpauthUrl } =
       await this.twoFactorAuthService.generateTwoFactorAuthSecret(user);
 
     return this.twoFactorAuthService.pipeQrCodeStream(response, otpauthUrl);
+  }
+
+  @Post("enable")
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  async enableTwoFactorAuth(
+    @currentUserFromBody() user: User,
+    @Body() { twoFactorAuthCode }: TwoFactorAuthCodeDto,
+  ) {
+    await this.validateCode(user, twoFactorAuthCode);
+    await this.twoFactorAuthService.enableTwoFactor(user);
+  }
+
+  @Post("authenticate")
+  @HttpCode(200)
+  async authenticate(
+    @currentUserFromBody() user: User,
+    @Body() { twoFactorAuthCode }: TwoFactorAuthCodeDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    await this.validateCode(user, twoFactorAuthCode);
+
+    const authCookie = this.authService.getCookieWithJwtToken(user.id);
+    response.setHeader("Set-Cookie", authCookie);
+  }
+
+  private async validateCode(user: User, twoFactorAuthCode: string) {
+    const isCodeValid = this.twoFactorAuthService.isTwoFactorAuthCodeValid(
+      twoFactorAuthCode,
+      user,
+    );
+    if (!isCodeValid) {
+      throw new UnauthorizedException("Wrong authentication code");
+    }
   }
 }
